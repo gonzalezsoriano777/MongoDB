6097d24a5284a8b0459f7a9af34f415c49029985
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-24 22:31:35 +0000

    introMongo

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ You can use it anytime you want to store data and persist it in your app
 Mongo is the "M" for Mean!
 
 */
+
+
+// In this course...
+/*
+
+What to focus on.. 
+
+Install MongoDB and Mongoose
+Use basic CRUD operations- Create, Read, Update and Delete
+Mocha (testing framework)
+
+*/
