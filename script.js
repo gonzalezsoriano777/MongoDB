a89996bb8ab108b9217ee7cd9e194aea69614d4d
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-24 22:23:54 +0000

    introMongo

---
+++ script.js
@@ -0,0 +1,12 @@
+// Notes
+/*
+
+What is MongoDB?
+
+It is a NoSQL Database
+You can use it anytime you want to store data and persist it in your app
+ 
+ Fun Fact:
+Mongo is the "M" for Mean!
+
+*/
