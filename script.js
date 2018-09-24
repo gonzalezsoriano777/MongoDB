0dca8e77d5eb9aded0518d0df6d8d4aac33df054
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-24 23:22:21 +0000

    lesson1(2)

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// Notes
+// Notes 
+
 /*
 
-What is MongoDB?
+How to install MongoDB(Overall review)
+How to install Mongoose and how it's gonna be used in the lesson itself
 
-It is a NoSQL Database
-You can use it anytime you want to store data and persist it in your app
- 
- Fun Fact:
-Mongo is the "M" for Mean!
-
-*/
-
-
-// In this course...
-/*
-
-What to focus on.. 
-
-Install MongoDB and Mongoose
-Use basic CRUD operations- Create, Read, Update and Delete
-Mocha (testing framework)
-
-*/
+/*/
