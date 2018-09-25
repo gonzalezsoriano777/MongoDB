ca0b75bf94b181dc564fbe3060b41262636410db
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-25 00:26:10 +0000

    lesso5

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 
 /*
 
-Doing collection for mario characters and Donkey Kong
-Schema is the details that should be expected
+What is Mocha
+
+-Mocha is a testing framework
+-Use it to perform tests within our application
+-Make sure everything works correctly
+
+This is gonna be used to test our connections with the database
+- Creating Records
+- Reading Records
+- Updating Records
+- Deleting Records
+
 
 */ 
