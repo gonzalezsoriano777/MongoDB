27440ca40910df0b554131f0a910ee572f2c983c
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-30 01:09:47 +0000

    lesson11

--- test/connection.js
+++ test/connection.js
@@ -1,40 +1,26 @@
-
+// New model
 const mongoose = require('mongoose');
 
 
+// ES6 Promises
 
-mongoose.Promise = global.Promise;
 
-
+// Connect to the db before tests run
 before(function(done){
   
 
 
-
+// Connect to mLab now
 mongoose.connect('mongodb://soriano123:hector3463@ds117623.mlab.com:17623/mongo', { useNewUrlParser: true }); // made one database called 'testaroo'
 
 //element.on('click', function(){ // Once the connection is open , it opens to that function
     
 //});
+
 mongoose.connection.once('open', function(){
   console.log('Connection has been made, now make fireworks...');
   done();
 }).on('error', function(error){
     console.log('Connection error:', error);
 });
-
-});
-
-
-
-
-// Drop the characters collection before each test
-
-beforeEach(function(done){
-  // Drop the collection
-  mongoose.connection.collections.mariokarts.drop(function(){ // going to the collection and saying "drop" the collection
-  // and it starts the test
-    done();
-  });
-
 });
