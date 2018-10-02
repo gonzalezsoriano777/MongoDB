895df9b5795dc0a8c07eb43f8069d34f7514bfad
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-10-02 22:41:52 +0000

    lesson13

--- test/updating_test.js
+++ test/updating_test.js
@@ -9,24 +9,23 @@ describe('Updating records', function(){
     beforeEach(function(done){
          char = new MarioChar({
            name: 'Mario'
-       });           
+    });           
         
         char.save().then(function(){ // waits for this to save first then does another action
             assert(char.isNew === false); // code would keep on running until it asserts something
             done(); // Lets mocha know the test is done and can keep on moving forward finding other test asserted
         }); 
-    });
+        });
     
     // Create test
     it('Updates one record from the database',function(done){
       
      MarioChar.findOneAndUpdate({name: 'Mario'}, {name: 'Luigi'}).then(function(){
-      MarioChar.findOne({_id: char._id}).then(function(result){
-          assert(result.name === 'Luigi');
-          done();
-      });
-         
-     });
+     MarioChar.findOne({_id: char._id}).then(function(result){
+     assert(result.name === 'Luigi');
+     done();
      
+       });
+      });
+     });
     });
-});
